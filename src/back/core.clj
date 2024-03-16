67fc0e0e3844a7950f7702eeb00e7f00ed71213c
Holdest White <zbsholdest@mail.ru>
2024-03-16 16:10:12 +0300

    image work

--- src/back/core.clj
+++ src/back/core.clj
@@ -14,20 +14,31 @@
             )
   (:gen-class))
 
+(defn add-post [params]
+  (println params)
+  )
+
+(defn add-comment [params]
+  (println params)
+  )
+
+;(defn add-post [{:keys [author-name title text image]}]
+;  (back.db-posts/add-post author-name title text image)
+;  )
+
+;(defn add-comment [{:keys [post-id author-name text image]}]
+;  (back.db-comments/add-comment (Integer/parseInt post-id) author-name text image)
+;  )
+
 (defroutes app-routes
            (GET "/posts" [] (constantly "hello"))
 
            (GET "/api/comments/get/:post-id" [post-id :<< as-int] (pr-str (back.db-comments/get-comments post-id)))
-           (GET "/api/comments/add/:post-id/:author-name/:text/:image"
-                [post-id :<< as-int author-name text image]
-             (pr-str (back.db-comments/add-comment post-id author-name text image))
-             )
+           (POST "/api/comments/add" [params] (pr-str (add-comment params)))
 
            (GET "/api/posts/get/:post-id" [post-id :<< as-int] (pr-str (back.db-posts/get-post post-id)))
-           (GET "/api/posts/add/:author-name/:title/:text/:image"
-                [author-name title text image]
-             (pr-str (back.db-posts/add-post author-name title text image))
-                )
+           (POST "/api/posts/add" [params] (pr-str (add-post params)))
+
            (route/not-found "Not Found")
            )
 
@@ -36,4 +47,4 @@
                         :access-control-allow-methods [:delete :get
                                                        :patch :post :put])
              (wrap-edn-params)
-             (wrap-defaults site-defaults)))
+             (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
